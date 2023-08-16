78f530015a0d49fa88f0fef0d820381aedf1362a
Deivid Fernando <deivid@grupoathosnegocios.com.br>
2023-08-16 01:11:05 -0300

    Entendendo o que são objetos no JS, sua diferenças com os arrays e como criá-los.

---
+++ aula1/objeto.js
@@ -0,0 +1,11 @@
+const listaCPFs = [111111, 22222222, 33333333];
+
+const informacoesPessoa = ['nome', 'Deivid', 'idade', 34, 'cpf', '07431356609'];
+
+const objetoPessoa = {
+    nome: 'Deivid',
+    idade: 34,
+    cpf: '07431356609',
+};
+
+console.log(objetoPessoa['nome']);
