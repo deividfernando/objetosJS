2f398b3daeb67b41105848eca5fe2fb96575912c
Deivid Fernando <deivid@grupoathosnegocios.com.br>
2023-08-17 01:13:19 -0300

    Nessa aula, aprendemos que podemos acessar os valores de um objeto utilizando objeto.NomedoCampo. Além disso, podemos manipular os valores do objeto para exibi-los como precisarmos, usando, por exemplo, o substring().

---
+++ aula1/acesso_ponto.js
@@ -0,0 +1,10 @@
+const cliente = {
+    nome: 'Deivid',
+    idade: 34,
+    cpf: '07431356609',
+    email: 'deivid.fernando88@gmail.com',
+};
+
+console.log(`O nome do cliente é ${cliente.nome} e essa pessoa tem ${cliente.idade} anos`);
+
+console.log(`Os três primeiros dígitos do CPF são ${cliente.cpf.substring(0,3)}`);
