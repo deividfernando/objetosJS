99e5929d9459e6bbc3544d4fc97a5231971fd254
Deivid Fernando <deivid@grupoathosnegocios.com.br>
2023-08-17 01:28:39 -0300

    Como adicionar novas informações dentro de um objeto, assim como alterar valores já existentes.

---
+++ aula1/alterar.js
@@ -0,0 +1,22 @@
+const pessoa = {
+    nome: 'Deivid',
+    profissao: 'DEV',
+};
+
+console.log(pessoa.nome);
+
+console.log(pessoa.telefone);
+
+pessoa.telefone = '(61) 98530-2526';
+
+console.log(pessoa.telefone);
+
+pessoa.nome = 'Deivid Fernando';
+
+console.log(pessoa);
+
+// const novaPessoa = {
+//     nome: 'Pedro',
+// }
+
+// pessoa = novaPessoa;
