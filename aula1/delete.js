7686ba7768cbe6822c9b5fc3bc6451ea336dcf69
Deivid Fernando <deivid@grupoathosnegocios.com.br>
2023-08-17 01:30:19 -0300

    Como deletar tanto o valor da propriedade quanto a chave de um objeto, utilizando o delete.

---
+++ aula1/delete.js
@@ -0,0 +1,18 @@
+const objPersonagem = {
+  nome: "Gandalf",
+  classe: "mago",
+  nivel: "20",
+  aliado: {
+    nome: "Saruman",
+    classe: "mago",
+  },
+  status: "desaparecido",
+};
+
+console.log(objPersonagem);
+
+delete objPersonagem.aliado;
+
+console.log(objPersonagem);
+
+console.log(objPersonagem.aliado)
