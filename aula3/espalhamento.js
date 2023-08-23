dd1521649bf18762be673940dd13a25b9e49263f
Deivid Fernando <deivid@grupoathosnegocios.com.br>
2023-08-22 21:50:04 -0300

    Mais alguns exemplos de utilização da sintaxe de espalhamento para juntar os objetos em apenas um.

--- aula3/espalhamento.js
+++ aula3/espalhamento.js
@@ -1,31 +1,46 @@
 const cliente = {
-    nome: "Joao",
-    idade: 24,
-    email: "joao@firma.com",
-    telefone: ["1155555550", "6198985630"],
-  };
-  
-  cliente.enderecos = [
-    {
-      rua: "Rua Milton Campos",
-      numero: 1337,
-      apartamento: true,
-      complemento: "ap 934",
-    },
-  ];
+  nome: "Joao",
+  idade: 24,
+  email: "joao@firma.com",
+  telefone: ["1155555550", "6198985630"],
+};
 
-  function ligaParaCliente(telefoneComercial, telefoneResidencial) {
+cliente.enderecos = [
+  {
+    rua: "Rua Milton Campos",
+    numero: 1337,
+    apartamento: true,
+    complemento: "ap 934",
+  },
+];
 
-    console.log(`Ligando para ${telefoneComercial}`);
-    console.log(`Ligando para ${telefoneResidencial}`);
-  }
+function ligaParaCliente(telefoneComercial, telefoneResidencial) {
+  console.log(`Ligando para ${telefoneComercial}`);
+  console.log(`Ligando para ${telefoneResidencial}`);
+}
 
-  //ligaParaCliente(cliente.telefone[0], cliente.telefone[1]);
-  ligaParaCliente(...cliente.telefone);
+//ligaParaCliente(cliente.telefone[0], cliente.telefone[1]);
+ligaParaCliente(...cliente.telefone);
 
-  const encomenda = {
-    destinatario: cliente.nome,
-    ...cliente.enderecos[0]
-  };
+const encomenda = {
+  destinatario: cliente.nome,
+  ...cliente.enderecos[0],
+};
 
-  console.log(encomenda);
+console.log(encomenda);
+
+const fichaGuerreiro = {
+  nome: "Aragorn",
+  classe: "guerreiro",
+};
+
+const equipoGuerreiro = {
+  espada: "Andúril",
+  capa: "capa élfica +2",
+};
+
+// const guerreiro = { fichaGuerreiro, equipoGuerreiro }
+// console.log(guerreiro)
+
+const guerreiro = { ...fichaGuerreiro, ...equipoGuerreiro }
+console.log(guerreiro)
